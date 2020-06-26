27121183ea4ea0e53b0023378cb7a47196e48b13
d-w-moore <dmoore@renci.org>
2020-06-26 22:13:01 +0000

    more myjansson corrections

--- json/myjansson.cpp
+++ json/myjansson.cpp
@@ -1,3 +1,5 @@
+#include <string> 
+#include <vector> 
 #include <memory>
 #include <cassert>
 #include <string>
@@ -20,20 +22,13 @@ const char* Json_dumps(Json_t *j)
   return std::strdup (s.c_str());
 }
 
-#define Json_string_value(j) Json_string_value_s(j,dummy_ )
+const char * Json_string_value_support  		\
+  std::vector<std::string> dummy_vec;			\
+  auto Json_string_value = [&dummy_vec] (Json_t* j) { 	\
+    d.emplace_back( j );				\
+    return d.back().c_str();				\
+  }
 
-const char* Json_string_value_s(Json_t *j,std::string & dummy) {
-   dummy = j->get<std::string>();
-   return dummy.c_str();
-}
-
-define Json_string_value(j) (
-	{
-		std::string dummy_string;
-		Json_string_value_s( (j), dummy_string);
-		std::strdup ( dummy_string.c_str() );
-	}
-)
 
 Json_t* Json_string(const char *s) {
   auto sg =  new Json_t ;
@@ -59,47 +54,47 @@ int Json_object_set ( Json_t * obj , const char* k , Json_t * value )
 
 Json_t* Json_pack_v( const std::string & Fmt , va_list v) ;
 
-#define ERASE_KEY_VALUE do { v = 0; k = 0; } while(0)
-#define DEALLOC_KEY_VALUE do { Json_decref(v); v = 0; delete k; k = 0; } \
-                             while(0)
 
-Json_t* Json_pack_v( const std::string & Fmt, ssize_t& s, va_list v) 
+Json_t* Json_pack_varg( const std::string & Fmt, ssize_t& s, va_list vl ) 
 {
-  Json_t *value = nullptr;
-  Json_t *key = nullptr;
+  Json_string_value_support;
+
+  Json_t* value = nullptr;
+  Json_t* key = nullptr;
   Json_t *obj = Json_null();
+
   bool Quit = false, Error = false;
 
   for (auto sp = fmt.begin(); sp != fmt.end(); ++sp) {
   {
     double d;
     auto c = *sp;
+    const char* key = 0;
     void *v;
-    std::string* key = 0;
     Json_t *j = 0;
     ssize_t incr = 1;
     switch (c) {
       case '{': 
         if (sp != Fmt.begin())
-          value = Json_pack_v (Fmt.substr(sp - Fmt.begin()), incr, v);
+          value = Json_pack_varg (Fmt.substr(sp - Fmt.begin()), incr, vl);
         else
           obj = Json_object();
         break;
-      case ':': key = new std::string{ Json_string_value ;  Json_decref( value ); value = nullptr;
+      case ':': key = new std::string{ Json_string_value(value) ; value = nullptr;
         break;
       case '}': sp++;  Quit = true; 
-      case ',': if (key && value) Json_set_object ()
+      case ',': if (key && value) Json_object_set (obj,key,value);
                 key = nullptr; value = nullptr;
         break;
-      case 'f': d = va_arg(arg, double); // floats promoted to double in '...'
+      case 'f': d = va_arg(vl, double); // floats promoted to double in '...'
                 if (!value) value = Json_real( d );
                 else Quit = Error = true;
         break;
-      case 'o': v = va_arg(arg, void*);
+      case 'o': v = va_arg(vl, void*);
                 if (!value) value = static_cast<Json_t*>(v);
                 else Quit = Error = true;
         break;
-      case 's': v = va_arg(arg, const char*);
+      case 's': v = va_arg(vl, const char*);
                 if (!value) value = Json_string (static_cast<const char*>(v));
                 else Quit = Error = true;
         break;
@@ -108,7 +103,6 @@ Json_t* Json_pack_v( const std::string & Fmt, ssize_t& s, va_list v)
     sp += incr;
     if (!v || Quit)  { break; }
   }
-  va_end(arg);
   s = sp - Fmt.begin();
   return obj ? obj : value;
 }
@@ -117,11 +111,10 @@ Json_t* Json_pack( const char* fmt , ...)
 {
   ssize_t s = 0;
   Json_t * p = nullptr;
-  std::string Fmt  { fmt };
-  std::va_list v;
-  va_start( v, fmt);
-  ssize_t s = Json_pack_v(Fmt, s, v);
-  va_end(v);
+  std::va_list vl;
+  va_start( vl, fmt);
+  ssize_t s = Json_pack_varg( fmt, s, vl );
+  va_end(vl);
   return p;
 }
 
