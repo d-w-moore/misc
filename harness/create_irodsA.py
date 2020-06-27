253c73f01240a24d8ae5bf2b7168b77a5c1ef383
d-w-moore <dmoore@renci.org>
2020-06-27 17:43:15 +0000

    create irodsA from python

---
+++ harness/create_irodsA.py
@@ -0,0 +1,19 @@
+import tempfile
+import subprocess
+import getpass
+
+def create_auth_file(pw):
+  p = None
+  with tempfile.SpooledTemporaryFile() as f:
+    f.write(pw + '\n')
+    f.seek(0)
+    f.read()
+    f.seek(0)
+    null=open('/dev/null','w')
+    p = subprocess.Popen('iinit',stdout=null,stderr=null,stdin=f)        
+    p.communicate()
+  return (p.returncode if p else None)
+
+if __name__ == '__main__':
+  s = getpass.getpass('->')
+  print create_auth_file (s)
