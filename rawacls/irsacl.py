382d9b21cacf4f18c620ef794de17bf380c4b417
d-w-moore <dmoore@renci.org>
2020-07-20 08:24:46 -0400

    move users_by_id function

--- rawacls/irsacl.py
+++ rawacls/irsacl.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+from __future__ import print_function
 import atexit
 import os
 import sys
@@ -7,14 +7,16 @@ import ssl
 import irods.exception
 import unittest
 import time
+import pprint
 
 from irods.meta import iRODSMeta
-from irods.column import Like
-from irods.models import DataObject,User,DataAccess, CollectionAccess  , Collection, CollectionUser
+from irods.models import (DataObject, User, DataAccess, CollectionAccess, Collection, CollectionUser)
 from irods.session import iRODSSession
 from irods.data_object import (irods_basename, irods_dirname , iRODSDataObject)
 from irods.collection import  iRODSCollection
 from irods.access import iRODSAccess
+from irods.column import In
+from irods.user import iRODSUser
 
 session = None
 
@@ -29,16 +31,31 @@ def get_session ():
         atexit.register(lambda : session.cleanup() if session else None)
     return session
 
-def coll_access_query(path) : 
+
+def coll_access_query(session,path):
   return session.query(Collection, CollectionAccess).filter(Collection.name == path)
-  
-def data_access_query(path) : 
+
+def data_access_query(session,path):
   cn = irods_dirname(path)
   dn = irods_basename(path)
-  return session.query(DataObject,  DataAccess).filter( Collection.name == cn, DataObject.name == dn ) 
+  return session.query(DataObject,  DataAccess).filter( Collection.name == cn, DataObject.name == dn )
 
+def users_by_ids(session,ids=()) :
+  try:
+    ids=list(iter(ids))
+  except TypeError:
+    if type(ids) in (str,) + six.integer_types: ids=int(ids)
+    else: raise
+  cond = () if not ids \
+         else (In(User.id,list(map(int,ids))),) if len(ids)>1 \
+         else (User.id == int(ids[0]),)
+  return [ iRODSUser(session.users,i)
+           for i in session.query(User.id,User.name,User.type,User.zone).filter(*cond) ]
 
-def get_acls_on_object( obj ):
+def get_acls_on_object( sess, obj , **kw ):
+
+    users_out = kw.pop( 'acl_users', None )
+    T = kw.pop( 'acl_users_transform', lambda value : value )
 
     if isinstance(obj, iRODSDataObject):
         access_column = DataAccess
@@ -49,41 +66,55 @@ def get_acls_on_object( obj ):
         access_column = CollectionAccess
         object_column = Collection
         query_func    = coll_access_query
-
     else:
         raise TypeError
 
-    rows  = [ r for r in query_func(obj.path) ]
+    rows  = [ r for r in query_func(sess, obj.path) ]
     userids = set( r[access_column.user_id] for r in rows )
-    
-    from getusers import users_by_ids
 
-    users_by_id = { j.id:j for j in users_by_ids(s, userids) }
+    user_lookup = { j.id:j for j in users_by_ids(sess, userids) }
+
+    if isinstance(users_out, dict):     users_out.update (user_lookup)
+    elif isinstance (users_out, list):  users_out += [T(v) for v in user_lookup.values()]
+    elif isinstance (users_out, set):   users_out |= set(T(v) for v in user_lookup.values())
+    elif users_out is None: pass
+    else:                   raise TypeError
 
     acls = [ iRODSAccess ( r[access_column.name],
-                           obj.path, 
-                           users_by_id[r[access_column.user_id]].name,
-                           users_by_id[r[access_column.user_id]].zone  ) for r in rows ]
-
+                           obj.path,
+                           user_lookup[r[access_column.user_id]].name,
+                           user_lookup[r[access_column.user_id]].zone  ) for r in rows ]
     return acls
 
+
+def main(sess, u=None):
+    obj = None
+    if u is not None: ulst = eval(u)
+    for tp in ['data object','collection']:
+        if tp == 'data object':
+            try:
+                obj = sess.data_objects.get(sys.argv[1])
+            except: pass
+        elif tp == 'collection':
+            try:
+                obj = sess.collections.get(sys.argv[1])
+            except: pass
+
+        if obj:
+            kw  = { 'acl_users': ulst,
+                   #'acl_users_transform':lambda x:x.id
+            } if 'ulst' in locals() else {}
+
+            acls = get_acls_on_object (sess, obj,**kw) # acl_users = ulst
+            pprint.pprint (acls)
+            print ('--- user output ---',)
+            pprint.pprint(ulst)
+            break
+        else :
+            print('no {} found...'.format(tp),file=sys.stderr)
 
 if __name__ == '__main__':
 
     s = get_session()
-    obj = None
+    main(s,*sys.argv[2:])
 
-    try:
-      obj = s.data_objects.get(sys.argv[1])
-    except: pass
-
-    try:
-      obj = s.collections.get(sys.argv[1])
-    except: pass
-
-    if obj:
-      acls = get_acls_on_object (obj)
-      import pprint
-      pprint.pprint(acls)
-    else :
-      print('no object found')
