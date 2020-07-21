d6e4fa0f2f2675e73c0b043f8aabaa730bd3f233
d-w-moore <dmoore@renci.org>
2020-07-21 19:24:38 +0000

    using pam / pw-obfusc

--- harness/pamargs.py
+++ harness/pamargs.py
@@ -1,5 +1,6 @@
 import json
 import atexit
+import sys
 import os
 import ssl
 import irods.exception
@@ -9,8 +10,31 @@ import socket
 import pprint
 from socket import gethostname
 from irods.session import iRODSSession
+import irods.password_obfuscation
+
+import irods.connection
+import  getopt
+
+demo_user =  'alissa'
+demo_passw = 'test123'
 
 
+opt,arg = getopt.getopt( 
+    sys.argv[1:], '', ['output=',
+                       'environment-directory=']) 
+
+env_dir = ('~/')
+out_filename = ''
+
+for key,val in opt:
+    if key == '--output':		 out_filename = val
+    if key == '--environment-directory': env_dir = val
+
+env_dir = os.path.expanduser( env_dir )
+
+#--------------
+
+irods.connection.Connection.DISALLOWING_PAM_PLAINTEXT = False
 
 ses = iRODSSession( 
         host=gethostname(),
@@ -20,6 +44,14 @@ ses = iRODSSession(
 	password='test123',
 	port= 1247
 )
+
+pam_hash = ses.pam_pw_negotiated
+
+if pam_hash: 
+    print pam_hash[0]
+    native_mode_encoded = irods.password_obfuscation.encode( pam_hash[0] )
+    open(out_filename,'wb').write(native_mode_encoded)
+
 coll_name = '/{ses.zone}/home/{ses.username}'.format(**locals())
 homecol = ses.collections.get(coll_name)
 print homecol
